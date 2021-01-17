6d7c20a32cb141370320fbe4925b9d1521f227df
khurin15 <54048999+khurin15@users.noreply.github.com>
2021-01-17 13:03:50 +0700

    create.php untuk uts

---
+++ create.php
@@ -0,0 +1,7 @@
+<form action ="insert.php" method="POST">
+    id_barang : <input type = "text" name="id_barang">
+    nama_barang : <input type = "text" name="nama_barang">
+    harga_barang : <input type="text" name="harga_barang">
+<button type= "submit">Simpan </button>
+
+</form  >
